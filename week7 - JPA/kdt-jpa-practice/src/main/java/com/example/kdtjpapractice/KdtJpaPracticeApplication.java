f73cb60eeec962404b96eda18c134732500d2b15
youngji <youngji.jang804@gmail.com>
2022-12-07 06:43:56 +0900

    jpa 세팅

---
+++ week7 - JPA/kdt-jpa-practice/src/main/java/com/example/kdtjpapractice/KdtJpaPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.kdtjpapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdtJpaPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KdtJpaPracticeApplication.class, args);
+    }
+
+}
---
+++ week7 - JPA/kdt-jpa-practice/src/test/java/com/example/kdtjpapractice/domain/CustomerRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.example.kdtjpapractice.domain;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+class CustomerRepositoryTest {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Test
+    void test(){
+        //Given
+        Customer customer = new Customer();
+        customer.setId(1L);
+        customer.setFirstName("youngji");
+        customer.setLastName("jang");
+
+        //When
+        customerRepository.save(customer);
+
+        //Then
+        Customer entity = customerRepository.findById(1L).get();
+        log.info("{} {}", entity.getFirstName(), entity.getLastName());
+    }
+
+
+}
