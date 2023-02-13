63283054467efad225e7a00e7315c14fc6312ac8
youngji <youngji.jang804@gmail.com>
2023-02-13 19:13:23 +0900

    cloud

---
+++ week - Cloud/citron/src/main/java/com/example/citron/CitronApplication.java
@@ -0,0 +1,13 @@
+package com.example.citron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitronApplication.class, args);
+	}
+
+}
---
+++ week - Cloud/citron/src/test/java/com/example/citron/CitronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.citron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
